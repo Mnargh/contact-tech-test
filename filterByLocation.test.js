7d46724e6f09b877a7522b790e2927402ed8b42d
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-31 18:58:33 +0000

    Added test for console output of filter by location results

--- filterByLocation.test.js
+++ filterByLocation.test.js
@@ -1,5 +1,8 @@
-const filterByLocation = require('./filterByLocation');
+const filterModule = require('./filterByLocation');
 const exampleData = require('./fixtures/models.json');
+
+const filterByLocation = filterModule.filterByLocation;
+const displayFilterResults = filterModule.displayFilterResults;
 
 
 test('FilterByLocation returns expected data given a location and multiple matches', () => {
@@ -19,4 +22,12 @@ test('FilterByLocation returns expected data given a location and multiple match
   expect(filterByLocation(exampleData, 'springfield')).toEqual(expectedResult)
   expect(filterByLocation(exampleData, 'Springfield')).toEqual(expectedResult)
   expect(filterByLocation(exampleData, 'SpringField')).toEqual(expectedResult)
+})
+
+test('Logs the correct output from filtered models data', () => {
+  console.log = jest.fn()
+  displayFilterResults(exampleData, 'Springfield')
+
+  expect(console.log.mock.calls[0][0]).toBe('Homer Simpson')
+  expect(console.log.mock.calls[1][0]).toBe('Krusty the Clown')
 })
