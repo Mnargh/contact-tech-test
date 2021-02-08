61de33532d91f42ca57bb524ced00069cf8d7a79
Tom Brandon <tombrandon@Toms-MacBook-Pro.local>
2021-02-08 21:46:13 +0000

    installing babel

--- getModelsInLocation.js
+++ getModelsInLocation.js
@@ -24,27 +24,27 @@ const fillMissingNames = (data) => {
 }
 
 const filterDataByLocation = (data, location) => {
-  // return data.filter(model => {
-  //   const modelLocation = model.location.toString()
-  //   return modelLocation.toLowerCase() == location.toLowerCase()
-  // })
+  return data.filter(model => {
+    const modelLocation = model.location.toString()
+    return modelLocation.toLowerCase() == location.toLowerCase()
+  })
 }
 
 const displayFilterResults = (filterResults, location) => {
-  // filterResults.length ? filterResults.forEach(model => {
-  //   console.log(model.name)
-  // }) : console.log(`No results were found for the location: ${location}`)
+  filterResults.length ? filterResults.forEach(model => {
+    console.log(model.name)
+  }) : console.log(`No results were found for the location: ${location}`)
 }
 
 const getModelsInLocation = (data, location) => {
-  // // validate data
-  // validateIsJSON(data)
-  // // correct missing name data
-  // const filledData = fillMissingNames(data)
-  // // filter the data
-  // const filteredData = filterDataByLocation(filledData, location)
-  // // display the result
-  // displayFilterResults(filteredData, location)
+  // validate data
+  validateIsJSON(data)
+  // correct missing name data
+  const filledData = fillMissingNames(data)
+  // filter the data
+  const filteredData = filterDataByLocation(filledData, location)
+  // display the result
+  displayFilterResults(filteredData, location)
 }
 
 export { getModelsInLocation, displayFilterResults, filterDataByLocation, fillMissingNames }
--- getModelsInLocation.test.js
+++ getModelsInLocation.test.js
@@ -5,82 +5,80 @@ import {
   getModelsInLocation
 } from './getModelsInLocation';
 
-import * as exampleJSON from './fixtures/models.json';
-// const exampleData = { exampleJSON }
-import * as emptyDetailsData from './fixtures/emptyDetailsData.json';
-// const { emptyDetailsData } = emptyDetailsJSON
+const exampleData = require('./fixtures/models.json');
+const emptyDetailsData = require('./fixtures/emptyDetailsData.json');
 
 test.only('Auto fills if no name entered in input data, ', () => {
   console.log(emptyDetailsData)
 
-  // expect(fillMissingNames(emptyDetailsData)).toEqual(
-  //   [
-  //     {
-  //       "name": "No name entered",
-  //       "location": "Los Angeles",
-  //       "date_of_birth": "1980-03-19"
-  //     },
-  //     {
-  //       "name": "Bart Simpson",
-  //       "location": "New York",
-  //       "date_of_birth": "1980-03-19"
-  //     }
-  //   ]
-  // )
+  expect(fillMissingNames(emptyDetailsData)).toEqual(
+    [
+      {
+        "name": "No name entered",
+        "location": "Los Angeles",
+        "date_of_birth": "1980-03-19"
+      },
+      {
+        "name": "Bart Simpson",
+        "location": "New York",
+        "date_of_birth": "1980-03-19"
+      }
+    ]
+  )
 })
 
-// test('FilterDataByLocation returns expected data given a location and multiple matches', () => {
-//   const expectedResult = [
-//     {
-//       "name": "Homer Simpson",
-//       "location": "Springfield",
-//       "date_of_birth": "1956-05-12"
-//     },
-//     {
-//       "name": "Krusty the Clown",
-//       "location": "SpringField",
-//       "date_of_birth": "1957-10-29"
-//     }
-//   ]
+test('FilterDataByLocation returns expected data given a location and multiple matches', () => {
+  const expectedResult = [
+    {
+      "name": "Homer Simpson",
+      "location": "Springfield",
+      "date_of_birth": "1956-05-12"
+    },
+    {
+      "name": "Krusty the Clown",
+      "location": "SpringField",
+      "date_of_birth": "1957-10-29"
+    }
+  ]
 
-//   expect(filterDataByLocation(exampleData, 'springfield')).toEqual(expectedResult)
-//   expect(filterDataByLocation(exampleData, 'Springfield')).toEqual(expectedResult)
-//   expect(filterDataByLocation(exampleData, 'SpringField')).toEqual(expectedResult)
-// })
+  expect(filterDataByLocation(exampleData, 'springfield')).toEqual(expectedResult)
+  expect(filterDataByLocation(exampleData, 'Springfield')).toEqual(expectedResult)
+  expect(filterDataByLocation(exampleData, 'SpringField')).toEqual(expectedResult)
+})
 
-// test('Logs the correct output from filtered models data', () => {
+test('Logs the correct output from filtered models data', () => {
 
-//   const exampleFilteredData = [
-//     {
-//       "name": "Homer Simpson",
-//       "location": "Springfield",
-//       "date_of_birth": "1956-05-12"
-//     },
-//     {
-//       "name": "Krusty the Clown",
-//       "location": "SpringField",
-//       "date_of_birth": "1957-10-29"
-//     }
-//   ]
+  const exampleFilteredData = [
+    {
+      "name": "Homer Simpson",
+      "location": "Springfield",
+      "date_of_birth": "1956-05-12"
+    },
+    {
+      "name": "Krusty the Clown",
+      "location": "SpringField",
+      "date_of_birth": "1957-10-29"
+    }
+  ]
 
-//   console.log = jest.fn()
-//   displayFilterResults(exampleFilteredData)
+  console.log = jest.fn()
+  displayFilterResults(exampleFilteredData)
 
-//   expect(console.log.mock.calls[0][0]).toBe('Homer Simpson')
-//   expect(console.log.mock.calls[1][0]).toBe('Krusty the Clown')
-// })
+  expect(console.log.mock.calls[0][0]).toBe('Homer Simpson')
+  expect(console.log.mock.calls[1][0]).toBe('Krusty the Clown')
+})
 
-// test('Outputs message if no results were found for input location', () => {
-//   console.log = jest.fn()
-//   displayFilterResults([], 'London')
+test('Outputs message if no results were found for input location', () => {
+  console.log = jest.fn()
+  displayFilterResults([], 'London')
 
-//   expect(console.log.mock.calls[0][0]).toBe('No results were found for the location: London')
-// })
+  expect(console.log.mock.calls[0][0]).toBe('No results were found for the location: London')
+})
 
-// test('getModelsInLocation outputs correct names given some models data', () => {
-//   console.log = jest.fn()
-//   getModelsInLocation(exampleData, 'Springfield')
-//   expect(console.log.mock.calls[0][0]).toBe('Homer Simpson')
-//   expect(console.log.mock.calls[1][0]).toBe('Krusty the Clown')
-// })
+test('getModelsInLocation outputs correct names given some models data', () => {
+  console.log = jest.fn()
+  getModelsInLocation(exampleData, 'Springfield')
+  expect(console.log.mock.calls[0][0]).toBe('Homer Simpson')
+  expect(console.log.mock.calls[1][0]).toBe('Krusty the Clown')
+})
 
